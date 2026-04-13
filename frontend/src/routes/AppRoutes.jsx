ab08f69313173a5e93edfacbd95cbf883ce8aae4
swabrahnamutebi12-art <swabrahnamutebi12@gmail.com>
2026-04-13 23:21:55 +0300

    I added the internship details

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,9 +4,10 @@ import Signup from "../pages/auth/Signup";
 import StudentDashboard from "../pages/student/StudentDashboard";
 import SupervisorDashboard from "../pages/supervisor/SupervisorDashboard";
 import AdminDashboard from "../pages/admin/AdminDashboard";
-import Sidebar from "../Components/sidebar";
+import Sidebar from "../Components/Sidebar";
 import SubmitLog from "../pages/student/SubmitLog";
 import WeeklyLogs from "../pages/student/WeeklyLogs";
+import InternshipDetails from "../pages/student/InternshipDetails";
 const AppRoutes = () => {
     return (
         <BrowserRouter>
@@ -18,7 +19,8 @@ const AppRoutes = () => {
                 <Route path= "/submitlog" element = {<SubmitLog/>}/>
                 <Route path= "/student" element = {<StudentDashboard/>}/>
                 <Route path= "/supervisor" element = {<SupervisorDashboard/>}/>
-                <Route path= "admin/" element = {<AdminDashboard/>}/>
+                <Route path= "/admin" element = {<AdminDashboard/>}/>
+                <Route path= "/student/internship-details" element = {<InternshipDetails/>}/>
             </Routes>
         </BrowserRouter>
     );
