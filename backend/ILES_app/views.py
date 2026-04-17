0801297fc97cdffa923c6d477682c43639d17b3f
Prince 🤖👾👽 <princehopeasiimwe@gmail.com>
2026-04-17 21:51:24 +0300

    Add student profile API and update end point

--- backend/ILES_app/views.py
+++ backend/ILES_app/views.py
@@ -16,6 +16,7 @@ def signup(request):
     
 
 @api_view(['POST'])
+from Django.contrib.auth import authenticate
 def login(request):
     user = authenticate(
         username=request.data.get('username'),
@@ -25,3 +26,13 @@ def login(request):
     if user:
         return Response({"message": "Login successful", "role": user.role})
     return Response({"error": "Invalid credentials"})
+from rest_framework.decorators import api_view
+from. models import Student
+
+@api_view(['GET'])
+def get_student_profile(request):
+    student = student.objects.get(user=request.user)
+    return Response({"name": student.user.username,
+                     "course":student.course,
+                     "phone": student.phone,
+                     "student_no": student.student_no})
