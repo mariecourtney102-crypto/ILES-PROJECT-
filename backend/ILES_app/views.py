f7cd79cca152acd1bf67f745846c614d17754f87
Prince 🤖👾👽 <princehopeasiimwe@gmail.com>
2026-04-22 08:32:34 +0300

    import

--- backend/ILES_app/views.py
+++ backend/ILES_app/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models import Q
 
+from .models import InternshipPlacement
+
 
 from .models import InternshipPlacement, WeeklyLog, Evaluation
 from .serializers import ( CustomUserSerializer, 
