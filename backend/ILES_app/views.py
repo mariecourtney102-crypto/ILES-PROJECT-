3958bf4615881257f7c7b771bd6e12c71db8bc13
mariyah <mariecourtney102@gmail.com>
2026-04-23 01:36:08 +0300

    presented Student role

--- backend/ILES_app/views.py
+++ backend/ILES_app/views.py
@@ -20,7 +20,7 @@ from .serializers import ( CustomUserSerializer,
 @api_view(['GET'])
 def choose_role(request):
     return Response ({
-        "available_roles"
+        "available_roles" ["Student",,]
     })
 
 @api_view(['GET'])
