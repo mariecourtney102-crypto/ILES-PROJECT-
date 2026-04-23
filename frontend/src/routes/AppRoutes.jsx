06eb09e06d4d66b5495723ce4f9504cd38f34a7f
RODNEY <sennyangerodney864@gmail.com>
2026-04-23 17:31:36 +0300

    few changes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter , Routes , Route } from "react-router-dom";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import Login from "../pages/auth/Login";
 import Signup from "../pages/auth/Signup";
 import UserFeedback from "../pages/student/UserFeedback";
@@ -13,22 +13,23 @@ import InternshipDetails from "../pages/student/InternshipDetails";
 import ProtectedRoute from "./ProtectedRoute";
 
 const AppRoutes = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path= "/" element = {<Login/>}/>
-                <Route path= "/signup" element = {<Signup/>}/>
-                <Route path= "/weeklylogs" element = {<ProtectedRoute requiredRole="student"><WeeklyLogs/></ProtectedRoute>}/>
-                <Route path= "/submitlog" element = {<ProtectedRoute requiredRole="student"><SubmitLog/></ProtectedRoute>}/>
-                <Route path= "/student" element = {<ProtectedRoute requiredRole="student"><StudentDashboard/></ProtectedRoute>}/>
-                <Route path= "/supervisor" element = {<ProtectedRoute requiredRole="supervisor"><SupervisorDashboard/></ProtectedRoute>}/>
-                <Route path= "/supervisor/assigned-students" element = {<ProtectedRoute requiredRole="supervisor"><SupervisorAssignedStudents/></ProtectedRoute>}/>
-                <Route path= "/supervisor/feedback" element = {<ProtectedRoute requiredRole="supervisor"><SupervisorFeedback/></ProtectedRoute>}/>
-                <Route path= "/admin" element = {<ProtectedRoute requiredRole="admin"><AdminDashboard/></ProtectedRoute>}/>
-                <Route path= "/student/feedback" element = {<ProtectedRoute requiredRole="student"><UserFeedback/></ProtectedRoute>}/>
-                <Route path= "/student/internship-details" element = {<ProtectedRoute requiredRole="student"><InternshipDetails/></ProtectedRoute>}/>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/weeklylogs" element={<ProtectedRoute requiredRole="student"><WeeklyLogs /></ProtectedRoute>} />
+        <Route path="/submitlog" element={<ProtectedRoute requiredRole="student"><SubmitLog /></ProtectedRoute>} />
+        <Route path="/student" element={<ProtectedRoute requiredRole="student"><StudentDashboard /></ProtectedRoute>} />
+        <Route path="/supervisor" element={<ProtectedRoute requiredRole="supervisor"><SupervisorDashboard /></ProtectedRoute>} />
+        <Route path="/supervisor/assigned-students" element={<ProtectedRoute requiredRole="supervisor"><SupervisorAssignedStudents /></ProtectedRoute>} />
+        <Route path="/supervisor/feedback" element={<ProtectedRoute requiredRole="supervisor"><SupervisorFeedback /></ProtectedRoute>} />
+        <Route path="/admin" element={<ProtectedRoute requiredRole="admin"><AdminDashboard /></ProtectedRoute>} />
+        <Route path="/student/feedback" element={<ProtectedRoute requiredRole="student"><UserFeedback /></ProtectedRoute>} />
+        <Route path="/student/internship-details" element={<ProtectedRoute requiredRole="student"><InternshipDetails /></ProtectedRoute>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 export default AppRoutes;
