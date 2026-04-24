b8a284187da9d0c453e944fa7f22884422e616e6
RODNEY <sennyangerodney864@gmail.com>
2026-04-24 11:53:17 +0300

    the funal changes

--- backend/ILES_app/serializers.py
+++ backend/ILES_app/serializers.py
@@ -3,12 +3,12 @@ from rest_framework import serializers
 from .models import CustomUser, Student, Supervisor, Admin, InternshipPlacement, WeeklyLog, Evaluation, EvaluationCriteria
 
 class CustomUserSerializer(serializers.ModelSerializer):
-    course_title = serializers.CharField(required=False, write_only=True)
-    university_name = serializers.CharField(required=False, write_only=True)
-    year_of_study = serializers.IntegerField(required=False, write_only=True)
-    place_of_work = serializers.CharField(required=False, write_only=True)
-    department = serializers.CharField(required=False, write_only=True)
-    staff_ID = serializers.CharField(required=False, write_only=True)
+    course_title = serializers.CharField(required=False, write_only=True, allow_blank=True)
+    university_name = serializers.CharField(required=False, write_only=True, allow_blank=True)
+    year_of_study = serializers.IntegerField(required=False, write_only=True, allow_null=True)
+    place_of_work = serializers.CharField(required=False, write_only=True, allow_blank=True)
+    department = serializers.CharField(required=False, write_only=True, allow_blank=True)
+    staff_ID = serializers.CharField(required=False, write_only=True, allow_blank=True)
 
     class Meta:
         model = CustomUser 
@@ -60,29 +60,33 @@ class CustomUserSerializer(serializers.ModelSerializer):
     @transaction.atomic
     def create(self, validated_data):
         role = validated_data["role"]
+        profile_data = {}
 
-        student_data = {
-            "course_title": validated_data.pop("course_title", None),
-            "university_name": validated_data.pop("university_name", None),
-            "year_of_study": validated_data.pop("year_of_study", None),
-        }
-        supervisor_data = {
-            "place_of_work": validated_data.pop("place_of_work", None),
-            "department": validated_data.pop("department", None),
-            "staff_ID": validated_data.pop("staff_ID", None),
-        }
-        admin_data = {
-            "department": validated_data.pop("department", None),
-        }
+        if role == "student":
+            profile_data = {
+                "course_title": validated_data.pop("course_title", None),
+                "university_name": validated_data.pop("university_name", None),
+                "year_of_study": validated_data.pop("year_of_study", None),
+            }
+        elif role == "supervisor":
+            profile_data = {
+                "place_of_work": validated_data.pop("place_of_work", None),
+                "department": validated_data.pop("department", None),
+                "staff_ID": validated_data.pop("staff_ID", None),
+            }
+        elif role == "admin":
+            profile_data = {
+                "department": validated_data.pop("department", None),
+            }
 
         user = CustomUser.objects.create_user(**validated_data)
 
         if role == "student":
-            Student.objects.create(users=user, **student_data)
+            Student.objects.create(users=user, **profile_data)
         elif role == "supervisor":
-            Supervisor.objects.create(users=user, **supervisor_data)
+            Supervisor.objects.create(users=user, **profile_data)
         elif role == "admin":
-            Admin.objects.create(users=user, **admin_data)
+            Admin.objects.create(users=user, **profile_data)
 
         return user
 
