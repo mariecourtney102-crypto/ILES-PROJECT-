7a6b28cc248d5b3de220d275258c0370436664d5
Swabrah <swabrahnamutebi12@gmail.com>
2026-04-24 19:41:53 +0300

    .

--- backend/ILES_app/urls.py
+++ backend/ILES_app/urls.py
@@ -4,7 +4,7 @@ from .import views
 
 
 urlpatterns = [
-    
+    path('', views.login, name='home'),
     path('choose-role/', views.choose_role, name='choose_role'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
