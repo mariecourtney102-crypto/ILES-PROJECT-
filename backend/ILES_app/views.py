ae47186b6ce6a890e4d1a0d18a73b0a4c8a2bcb2
Eleanor Atim <atimeleanor@gmail.com>
2026-04-26 15:16:22 +0300

    Added student viewing their log

--- backend/ILES_app/views.py
+++ backend/ILES_app/views.py
@@ -176,3 +176,11 @@ def create_log(request):
         serializer.save(user=request.user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#STUDENT VIEWS THEIR LOG
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_logs(request):
+    logs = WeeklyLog.objects.filter(user=request.user)
+    serializer = WeeklylogSerializer(logs, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
