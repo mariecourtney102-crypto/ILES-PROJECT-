88e28e7b5614c4747fca9ea731dd76812943f42e
Eleanor Atim <atimeleanor@gmail.com>
2026-04-27 09:14:17 +0300

    Syncing with main

--- backend/ILES_app/urls.py
+++ backend/ILES_app/urls.py
@@ -6,4 +6,5 @@ from .import views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
+    path('criteria/', views.get_criteria, name= 'get-criteria')
 ]
