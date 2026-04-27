738f61cdd80af4ca5dd36c38fbaf580dac8a7dea
Eleanor Atim <atimeleanor@gmail.com>
2026-04-27 20:05:33 +0300

    Added the authentication urls

--- backend/ILES_app/urls.py
+++ backend/ILES_app/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
 from .import views
 
-
-
 urlpatterns = [
+    #Authentication
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
-    path('criteria/', views.get_criteria, name= 'get-criteria'),
-    path('', views.login, name='home'),
-    path('choose-role/', views.choose_role, name='choose_role'),
-    path('dashboard/', views.dashboard, name='dashboard')
+    path('logout/', views.logout_view, name='logout'),
 ]
