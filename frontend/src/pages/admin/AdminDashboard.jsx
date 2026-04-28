2e5668531b0a0243c22e6a1c21a359e49ac3a830
mariyah <mariecourtney102@gmail.com>
2026-04-28 09:24:46 +0300

    included the dashboard layout into the admin dashboard

--- frontend/src/pages/admin/AdminDashboard.jsx
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -1,7 +1,7 @@
 
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import DashboardLayout from "../../Components/dashboard_layout";
 const AdminDashboard = () => {
   const [stats, setStats] = useState(null);
 
@@ -13,19 +13,9 @@ const AdminDashboard = () => {
   }, []);
 
   return (
-    <div>
-      <h1 className="text-2xl font-bold text-teal-600 mb-4">
-        Dashboard
-      </h1>
+    <></>
 
-      <div className="bg-white p-4 rounded-lg shadow">
-        {stats ? (
-          <pre>{JSON.stringify(stats, null, 2)}</pre>
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
-    </div>
+
   );
 };
 
