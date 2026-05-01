92f33994bd7ce3cbf9a42dd8c5e16f48554474cd
RODNEY <sennyangerodney864@gmail.com>
2026-05-01 19:17:59 +0300

    i have registered the models in djangi admin

--- backend/ILES_app/admin.py
+++ backend/ILES_app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import (
+    CustomUser,
+    Student,
+    Supervisor,
+    Admin as AdminModel,
+    InternshipPlacement,
+    WeeklyLog,
+    EvaluationCriteria,
+    Evaluation,
+    Feedback,
+    SiteSetting,
+    Notification,
+)
 
-# Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Student)
+admin.site.register(Supervisor)
+admin.site.register(AdminModel)
+admin.site.register(InternshipPlacement)
+admin.site.register(WeeklyLog)
+admin.site.register(EvaluationCriteria)
+admin.site.register(Evaluation)
+admin.site.register(Feedback)
+admin.site.register(SiteSetting)
+admin.site.register(Notification)
