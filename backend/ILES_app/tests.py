31ecc78848616cf5bc5fe01e9612bb64da9bf368
Eleanor <atimeleanor@gmail.com>
2026-06-12 13:18:45 +0300

    final check

--- backend/ILES_app/tests.py
+++ backend/ILES_app/tests.py
@@ -150,7 +150,7 @@ class SupervisorAssignmentFlowTests(APITestCase):
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['name'], 'Alice Student')
         self.assertEqual(response.data[0]['course_title'], 'Computer Science')
-        self.assertEqual(response.data[0]['placement']['place_of_internship'], self.supervisor_company)
+        self.assertEqual(response.data[0]['placement']['place_of_internship'], 'Main Office')
 
 
 class InternshipPlacementTests(APITestCase):
