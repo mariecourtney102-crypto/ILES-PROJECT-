31551d887bea33a9ab4f1e1c59d393f28296b821
Swabrah <swabrahnamutebi12@gmail.com>
2026-06-13 23:07:41 +0300

    Tokens

--- backend/ILES_app/views.py
+++ backend/ILES_app/views.py
@@ -1,6 +1,5 @@
 from functools import wraps
 from unicodedata import name
-
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from django.http import JsonResponse
--- backend/ILES_project/settings.py
+++ backend/ILES_project/settings.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
+        ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
